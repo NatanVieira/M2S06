1cce2ee255eb1879a24483f2aaeaeac6f77dc23d
Natanael Vieira <natanaelvieira130@gmail.com>
2022-07-08 16:19:43 -0300

    M2S06 - Exercicio 08

---
+++ modulo2-semana6-api/Controllers/Exercicio8.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace modulo2_semana6_api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+
+public class Exercicio8 : ControllerBase {
+
+    [HttpGet("{valor1}/{valor2}")]
+    public int Get(int valor1, int valor2){
+        int resultado = 10;
+        try{
+            resultado = valor1 + valor2;
+            if(resultado >= 10)
+                throw new Exception("Maior ou igual a 10");
+        }
+        catch(Exception ex){
+
+        }
+        finally {
+            resultado = resultado < 10 ? resultado : new Random().Next();
+        }
+        return resultado;
+    }
+}
